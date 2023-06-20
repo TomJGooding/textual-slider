1be44bc3b2cf7057141fb193a72de34897a523b6
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-06-20 18:09:14 +0100

    feat: improve examples styling

--- examples/steps.py
+++ examples/steps.py
@@ -1,6 +1,6 @@
 from textual import on
 from textual.app import App, ComposeResult
-from textual.containers import Center
+from textual.containers import Center, Vertical
 from textual.widgets import Label
 
 from textual_slider.slider import Slider
@@ -11,20 +11,33 @@ class SliderWithStepApp(App):
     Screen {
         align: center middle;
     }
+
+    Vertical {
+        height: auto;
+        padding: 1 0;
+        content-align: center middle;
+    }
+
+    .title {
+        text-style: bold;
+    }
     """
 
     def compose(self) -> ComposeResult:
-        yield Center(Label("0 to 100 slider with step of 25"))
-        yield Center(Slider(min=0, max=100, step=25, value=0, id="slider1"))
-        yield Center(Label(id="slider1-value"))
+        with Vertical():
+            yield Center(Label("0 to 100 slider with step of 25", classes="title"))
+            yield Center(Slider(min=0, max=100, step=25, value=0, id="slider1"))
+            yield Center(Label(id="slider1-value"))
 
-        yield Center(Label("-20 to 20 slider with step of 4"))
-        yield Center(Slider(min=-20, max=20, step=4, value=0, id="slider2"))
-        yield Center(Label(id="slider2-value"))
+        with Vertical():
+            yield Center(Label("-20 to 20 slider with step of 4", classes="title"))
+            yield Center(Slider(min=-20, max=20, step=4, value=0, id="slider2"))
+            yield Center(Label(id="slider2-value"))
 
-        yield Center(Label("200 to 500 slider with step of 100"))
-        yield Center(Slider(min=200, max=500, step=100, value=200, id="slider3"))
-        yield Center(Label(id="slider3-value"))
+        with Vertical():
+            yield Center(Label("200 to 500 slider with step of 100", classes="title"))
+            yield Center(Slider(min=200, max=500, step=100, value=200, id="slider3"))
+            yield Center(Label(id="slider3-value"))
 
     @on(Slider.Changed, "#slider1")
     def on_slider_changed_slider1(self, event: Slider.Changed) -> None:
